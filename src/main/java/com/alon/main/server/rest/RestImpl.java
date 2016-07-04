912715c4cc97d81375f02f6879d100139761e7f1
alon_ss <alon.sarshalom@gmail.com>
2016-07-04 15:00:56 +0300

    Add Restfull service with jersy and spring

--- src/main/java/com/alon/main/server/rest/RestImpl.java
+++ src/main/java/com/alon/main/server/rest/RestImpl.java
@@ -1,31 +1,57 @@
 package com.alon.main.server.rest;
 
+import com.alon.main.server.dao.Dao;
+import com.alon.main.server.entities.Movie;
+import com.alon.main.server.entities.User;
 import com.alon.main.server.service.RecommenderService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/recommend/")
 @Component
 public class RestImpl {
 
-	@PostConstruct
-	private void init(){
-		Integer t = 0;
-		t.toString();
-	}
 
 	//	http://localhost:8090/recommend/test
 
 	@Autowired
 	public RecommenderService recommenderService;
 
+	@Autowired
+	public Dao<Movie> movieDao;
+
+	@Autowired
+	public Dao<User> userDao;
+
 	@GET
-//	@Path("{id}")
-	public Response getCombinedMsg() {
-		return Response.status(200).entity("zip").build();
+	@Path("{id}")
+	public List<Movie> getCombinedMsg(@PathParam("id") String id) {
+
+		Integer userInnerId = null;
+
+//		User user;
+//		if (ObjectId.isValid(id)){
+//			user = userDao.getById(new ObjectId(id));
+//			if (user != null){
+//				userInnerId = user.getInnerId();
+//			}
+//		}
+
+//		List<Integer> moviesInnerIds = userInnerId == null ?
+//				recommenderService.recommend():
+//				recommenderService.recommend(userInnerId);
+//
+
+		List<Integer> moviesInnerIds = recommenderService.recommend(Integer.valueOf(id));
+
+		List<Movie> movies = movieDao.getByInnerIds(moviesInnerIds);
+
+		return movies;
 	}
 }
--- src/main/java/com/alon/main/server/service/RecommenderService.java
+++ src/main/java/com/alon/main/server/service/RecommenderService.java
@@ -1,15 +1,21 @@
 package com.alon.main.server.service;
 
+import com.google.common.collect.Iterables;
+import org.apache.commons.math3.analysis.function.Identity;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaDoubleRDD;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.mllib.recommendation.ALS;
 import org.apache.spark.mllib.recommendation.MatrixFactorizationModel;
 import org.apache.spark.mllib.recommendation.Rating;
+import org.apache.spark.rdd.RDD;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import scala.Tuple2;
@@ -21,8 +27,10 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.alon.main.server.Const.Consts.*;
 
@@ -41,7 +49,7 @@ public final class RecommenderService {
     @Async
     private void init() {
         SparkConf sparkConf = new SparkConf().setAppName("JavaALS");
-        sparkConf.setMaster("local[2]").set("spark.executor.memory", "1g");
+        sparkConf.setMaster("local[8]").set("spark.executor.memory", "4g");
         sc = new JavaSparkContext(sparkConf);
         setModel();
     }
@@ -64,10 +72,49 @@ public final class RecommenderService {
         return ratingsList.stream().map(Rating::product).collect(Collectors.toList());
     }
 
+    public List<Integer> recommend() {
+        JavaRDD<Tuple2<Object, Rating[]>> userToRatings = model.recommendProductsForUsers(1).toJavaRDD();
+
+        JavaRDD<Rating> counvterToMovieId = userToRatings.map(Tuple2::_2).map(x -> x[0]);
+
+        long counvtwerToMovieId = userToRatings.map(Tuple2::_2).map(x -> x[0]).count();
+
+        JavaRDD<Tuple2<Integer, Integer>> counterToMovieId = userToRatings.
+                map(new UserRatingToMovieIds()).
+                flatMap(List::iterator).
+                groupBy(x -> x).
+                mapValues(Iterables::size).
+                map(x -> new Tuple2<>(x._2(), x._1));
+
+        List<Integer> response = JavaPairRDD.fromJavaRDD(counterToMovieId).
+                sortByKey(false).
+                take(RESPONSE_NUM).
+                stream().map(Tuple2::_2).
+                collect(Collectors.toList());
+
+
+        return response ; //ratingsList.stream().map(Rating::product).collect(Collectors.toList());
+    }
+
+
+    private static class UserRatingToMovieIds implements Function<Tuple2<Object, Rating[]>, List<Integer>> {
+
+        @Override
+        public List<Integer> call(Tuple2<Object, Rating[]> line) {
+
+            return Arrays.asList(line._2()).
+                    stream().
+                    map(Rating::product).
+                    collect(Collectors.toList());
+        }
+    }
+
 
     private void setModel() {
         Configuration conf = new Configuration();
 
+//        model = testSomeModels();
+//
         try {
             Path path = new Path(MODEL_PATH);
             long dateLong = FileSystem.get(conf).getFileStatus(path).getModificationTime();
@@ -104,6 +151,49 @@ public final class RecommenderService {
         return newModel;
     }
 
+
+
+    private MatrixFactorizationModel testSomeModels() {
+
+        JavaRDD<String> lines = sc.textFile(RATINGS_PATH);
+        String first = lines.first();
+        JavaRDD<Rating> ratings = lines.filter(line -> !line.equals(first)).map(new ParseRating());
+
+        // Build the recommendation model using ALS
+//        MatrixFactorizationModel model1 = MatrixFactorizationModel.load(sc.sc(), MODEL_PATH);
+        int rank = 10;
+        int numIterations = 10;
+        MatrixFactorizationModel model1 = ALS.trainImplicit(JavaRDD.toRDD(ratings), rank, numIterations);
+        model1.save(sc.sc(), MODEL_PATH);
+
+        double mean1 = testModel(ratings, model1);
+        Rating[] model1recommend = model1.recommendProducts(247754, 10);
+        System.out.println("Model1 Mean Squared Error = " + mean1);
+        System.out.println("Model1 recommend for products" );
+        Arrays.asList(model1recommend).stream().map(Rating::product).forEach(System.out::println);
+
+        return model;
+
+    }
+
+    private double testModel(JavaRDD<Rating> ratings, MatrixFactorizationModel model) {
+
+
+        JavaRDD<Tuple2<Object, Object>> userProducts = ratings.map(r -> new Tuple2<Object, Object>(r.user(), r.product()));
+
+                // Evaluate the model on rating data
+        JavaRDD<Tuple2<Tuple2<Integer, Integer>, Double>> predictionsNorPair = model.predict(JavaRDD.toRDD(userProducts)).toJavaRDD().map(r -> new Tuple2<>(new Tuple2<Integer, Integer>(r.user(), r.product()), r.rating()));
+        JavaPairRDD<Tuple2<Integer, Integer>, Double> predictions = JavaPairRDD.fromJavaRDD(predictionsNorPair);
+
+
+        JavaRDD<Tuple2<Double, Double>> ratesAndPreds = JavaPairRDD.fromJavaRDD(ratings.map(r -> new Tuple2<>(new Tuple2<Integer, Integer>(r.user(), r.product()), r.rating()))).join(predictions).values();;
+
+        Double MSE = ratesAndPreds.mapToDouble(pair -> (pair._1() - pair._2()) * (pair._1() - pair._2())).mean();
+
+        return MSE;
+
+    }
+
     private static class ParseRating implements Function<String, Rating> {
 
         @Override
@@ -119,18 +209,16 @@ public final class RecommenderService {
     private static void deleteAndAddAll(MatrixFactorizationModel model) {
         Configuration conf = new Configuration();
 
-        deleteAndAddDir(model, conf, USER_FEATURES_PATH);
-
-        deleteAndAddDir(model, conf, PRODUCT_FEATURES_PATH);
-
+        deleteAndAddDir(model.userFeatures().toJavaRDD(), conf, USER_FEATURES_PATH);
+        deleteAndAddDir(model.productFeatures().toJavaRDD(), conf, PRODUCT_FEATURES_PATH);
     }
 
-    private static void deleteAndAddDir(MatrixFactorizationModel model, Configuration conf, String pathString) {
+    private static void deleteAndAddDir(JavaRDD<Tuple2<Object, double[]>> model, Configuration conf, String pathString) {
         Path path = new Path(pathString);
         try {
 
             FileSystem.get(conf).delete(path, true);
-            model.userFeatures().toJavaRDD().map(RecommenderService::FeaturesToString).saveAsTextFile(pathString);
+            model.map(RecommenderService::FeaturesToString).saveAsTextFile(pathString);
         } catch (IOException e) {
             e.printStackTrace();
         }
